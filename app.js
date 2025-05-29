ed523b91534d2c7f5cfccad6ddb7b4ee14750fda
Katia Roseli Leite Pezuti <katia.rlpezuti@senacsp.edu.br>
2025-05-28 22:09:00 -0300

    app apos pack express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World");jj
+const express = require('express');
+//console.log("Hello World");
+const app = express();
+app.get("/", function(req, res){
+    res.write("Hello World");
+    res.end();
+});
+
+app.listen(8080);

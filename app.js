e8cfc68f0192b20dd8378836b8177fd27810160d
Katia Roseli Leite Pezuti <katia.rlpezuti@senacsp.edu.br>
2025-06-04 22:13:06 -0300

    adicionado interface mysql2

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro){
+    if (erro){
+        console.error('Erro ao conectar ao banco de dado:', erro);
+        return;
+    }
+    console.log('Conexao com o banco de dados estabelecida com sucesso!');
+});
+
 app.get("/", function(req, res){
-    res.write("Hello World");
+    res.write("Hello Worldddddddddddd");
     res.end();
 });
 

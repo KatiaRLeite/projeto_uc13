db6a951b0a3ce2e378ccaa8f5b8e6d8238a1bea3
Katia Roseli Leite Pezuti <katia.rlpezuti@senacsp.edu.br>
2025-06-12 22:13:35 -0300

    drop down categoria

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const {engine}=require('express-handlebars');
 const mysql = require('mysql2');
 
 app.use('/bootstrap', express.static(__dirname+'/node_modules/bootstrap/dist'));
+app.use('/static', express.static(__dirname+'/static'));
+
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -26,7 +28,7 @@ conexao.connect(function(erro){
     console.log('Conexao com o banco de dados estabelecida com sucesso!');
 });
 
-app.get("/index", function(req, res){
+app.get("/", function(req, res){
     let sql='SELECT * FROM produtos';
     conexao.query(sql,function(erro,produtos_qs){
     if (erro){
@@ -49,6 +51,19 @@ app.get("/clientes", function(req, res){
     }    
 
     res.render('clientes',{clientes: clientes_qs});
+});
+});
+
+app.get("/produto_inc", function(req, res){
+    let sql='SELECT * FROM categorias';
+    conexao.query(sql,function(erro,categoria_qs){
+    if (erro){
+        console.error('Erro ao consultar categoria:',erro);
+        res.status(500).send('Erro ao consultar categoria');
+        return;
+    }    
+
+    res.render('produto_inc',{categoria: categoria_qs});
 });
 });
 
